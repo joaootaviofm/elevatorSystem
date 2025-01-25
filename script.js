eb4f943701a1e1db4f7874fb77b35cdac4b1b63c
joaootaviofm <joaootaviofm@gmail.com>
2025-01-25 14:12:28 -0300

    reset projeto

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var firstDoor = document.getElementById('entrance1')
 var secondDoor = document.getElementById('entrance2')
 var thirdDoor = document.getElementById('entrance3')
-var toFirstFloor = document.getElementById('andar')
-
-
-toFirstFloor.addEventListener('click', () => {
-    firstDoor.style.marginBottom = "150px"
-})
 
 
 
+
+
+// 00 = terreo
 // 150 px = first floor
 // 300 px = second floor
 // 450px = third floor
 // 600px = 4th floor 
-// 750px = last floor
+// 750px = cobertura
+
+// elevator transition timing
