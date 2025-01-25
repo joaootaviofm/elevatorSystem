6038f3deb47d4872781cbb211c598076679bbbce
joaootaviofm <joaootaviofm@gmail.com>
2025-01-25 10:50:49 -0300

    teste botão ease transition

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var firstDoor = document.getElementById('entrance1')
 var secondDoor = document.getElementById('entrance2')
 var thirdDoor = document.getElementById('entrance3')
+var toFirstFloor = document.getElementById('andar')
 
 
-
+toFirstFloor.addEventListener('click', () => {
+    firstDoor.style.marginBottom = "150px"
+})
 
 
 
