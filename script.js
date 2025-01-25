5cfc06bf42c8ea2a899e578ab5954c28ed0288b1
joaootaviofm <joaootaviofm@gmail.com>
2025-01-25 10:20:49 -0300

    pegando id das portas

---
+++ script.js
@@ -0,0 +1,14 @@
+var firstDoor = document.getElementById('entrance1')
+var secondDoor = document.getElementById('entrance2')
+var thirdDoor = document.getElementById('entrance3')
+
+
+
+
+
+
+// 150 px = first floor
+// 300 px = second floor
+// 450px = third floor
+// 600px = 4th floor 
+// 750px = last floor
